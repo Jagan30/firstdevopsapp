433591f616bec77c7c2a7cbf0a01116da5fd1515
J8118287 <Jaganathan.s@saint-gobain.com>
2023-12-02 11:50:18 +0530

    api

--- firstdevopsapp/Controllers/WeatherForecastController.cs
+++ firstdevopsapp/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace firstdevopsapp.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Jagan","Virat"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
